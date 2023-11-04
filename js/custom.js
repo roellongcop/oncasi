23a0c19eda249c64fcf704d6a7648a1e3028c1c3
roellongcop <longcoproel@gmail.com>
2023-11-04 12:20:18 +0800

    c

--- js/custom.js
+++ js/custom.js
@@ -3,13 +3,11 @@ document.getElementById('openDrawerBtn').addEventListener('click', function () {
   // Prevent the event from bubbling up to the document
   event.stopPropagation();
 });
-
 document.getElementById('closeDrawerBtn').addEventListener('click', function () {
   document.getElementById('sideDrawer').classList.remove('open');
   // Prevent the event from bubbling up to the document
   event.stopPropagation();
 });
-
 // Close the drawer when clicking outside of it
 document.addEventListener('click', function (event) {
   var sideDrawer = document.getElementById('sideDrawer');
@@ -17,7 +15,6 @@ document.addEventListener('click', function (event) {
     sideDrawer.classList.remove('open');
   }
 });
-
 // Optional: Prevent clicks inside the drawer from closing it
 document.getElementById('sideDrawer').addEventListener('click', function (event) {
   event.stopPropagation();
